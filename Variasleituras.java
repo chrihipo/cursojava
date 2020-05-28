f466c4588fe5edab05e8ae2e0fbb885100097366
chrihipo <chrihipo@gmail.com>
2020-05-28 17:35:39 -0300

    Atualizado

--- Variasleituras.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Uri1003{
-    public static void main(String args[]){
-        Scanner teclado = new Scanner(System.in);
-        int numero;
-        String nome;
-        //qual 
-
-        
